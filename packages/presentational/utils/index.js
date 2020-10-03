cff711e2e4c0f3fb0a207ff0682376d5be2b96c3
shashank <shashankmishrra@gmail.com>
2020-10-03 19:21:42 +0530

    added a math util

---
+++ packages/presentational/utils/index.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * @param {string|number} a
+ * @param {string|number} b
+ */
+const add = (a, b) => a + b;
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ */
+const sub = (a, b) => a - b;
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ */
+const mult = (a, b) => a * b;
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ */
+const div = (a, b) => a / b;
+
+module.exports = {
+  add,
+  sub,
+  mult,
+  div,
+};
